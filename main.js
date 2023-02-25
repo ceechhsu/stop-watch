62d4ac2faf263ea40e6d3dc56ba96a9325a4cf18
Ceech Hsu <ceechhsu@gmail.com>
2023-02-25 12:11:43 -0800

    rid of hh mm ss

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 const controls = document.querySelectorAll(".controls button");
+
+const getFromLS = (name) => {
+  if (localStorage.getItem(name) == null) {
+    localStorage.setItem(name, 0);
+  }
+  return JSON.parse(localStorage.getItem(name));
+};
+
+const storeToLS = (aName, aInput) => {
+  const bInput = JSON.stringify(aInput);
+  localStorage.setItem(aName, bInput);
+};
+
 for (let i = 0; i < controls.length; i++) {
   controls[i].addEventListener("click", () => {
     switch (i) {
@@ -17,8 +30,12 @@ for (let i = 0; i < controls.length; i++) {
 
 const startClock = () => {
   // 1) get the time from LS
+  let currentTime = getFromLS("storedTime");
+
   // 2) start incrementing the time
+  const intervalID = setInterval(currentTime++, 1000);
   // 3) display the time
+  console.log(currentTime);
 };
 
 const stopClock = (type) => {
